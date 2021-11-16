af403bea50ae488dd364cfbf24b0c10015c74bee
Pramod Lawate <Pramod.Lawate@btl.com>
2021-11-16 20:27:19 +0000

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileTransform.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.appsetting  = ConfigurationManager.AppSettings["Environment"].ToString();
+            ViewBag.connection = ConfigurationManager.ConnectionStrings["con"].ToString();
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
